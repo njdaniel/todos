fb589ea19df282470a9434ed4eb69babd46528af
njdaniel <nicholasjdaniel@gmail.com>
2017-07-06 16:31:20 -0400

    add node uuid

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,11 +12,11 @@
 // };
 // EQUALS the other 'addTodo' function
 
+import { v4 } from 'node-uuid';
 
-let nextTodoId = 0;
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
-  id: (nextTodoId++).toString(),
+  id: v4(),
   text,
 });
 

f2a189beaffa1ffbf518dbb0c003f1ac0722953d
njdaniel <nicholasjdaniel@gmail.com>
2017-07-06 15:09:11 -0400

    refactor func to object expressions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,31 @@
+// Using Object Expression to change fat arrow functions
+// to be surrounded with () instead of using 'return {}'
+
+// EX
+// export const addTodo = (text) => {
+//   return {}
+//     type: 'ADD_TODO',
+//     id: (nextTodoId++).toString(),
+//     text,
+//   };
+// };
+// EQUALS the other 'addTodo' function
+
+
 let nextTodoId = 0;
-export const addTodo = (text) => {
-  return {
-    type: 'ADD_TODO',
-    id: (nextTodoId++).toString(),
-    text,
-  };
-};
+export const addTodo = (text) => ({
+  type: 'ADD_TODO',
+  id: (nextTodoId++).toString(),
+  text,
+});
 
-export const setVisibilityFilter = (filter) => {
-  return {
-    type: 'SET_VISIBILITY_FILTER',
-    filter,
-  };
-};
+export const setVisibilityFilter = (filter) => ({
+  type: 'SET_VISIBILITY_FILTER',
+  filter,
+});
 
-export const toggleTodo = (id) => {
-  return {
-    type: 'TOGGLE_TODO',
-    id,
-  };
-};
+export const toggleTodo = (id) => ({
+  type: 'TOGGLE_TODO',
+  id,
+});
+

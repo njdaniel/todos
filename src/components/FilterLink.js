5c11f7a9989c511742f795a694d2444ca0eb7de8
njdaniel <nicholasjdaniel@gmail.com>
2017-07-06 15:15:54 -0400

    concise notation

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -6,8 +6,17 @@ const mapStateToProps = (state, ownProps) => ({
   active: ownProps.filter === state.visibilityFilter,
 });
 
+
+// const mapDispatchToProps = (dispatch, ownProps) => ({
+//     onClick: () => {
+//         dispatch(setVisibilityFilter(ownProps.filter));
+//     },
+// });
+// When only a function is being returned in a function that is a function you can condense to below
+// Fat arrow to concise when a function is inside an object
+
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: () => {
+  onClick() {
     dispatch(setVisibilityFilter(ownProps.filter));
   },
 });

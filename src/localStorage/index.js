dbdc5852b0b827f73a88d0aedb609b8c5b913b75
njdaniel <nicholasjdaniel@gmail.com>
2017-07-06 16:14:33 -0400

    add save state

--- src/localStorage/index.js
+++ src/localStorage/index.js
@@ -12,3 +12,12 @@ export const loadState = () => {
     return undefined;
   }
 };
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    // Ignore write error
+  }
+};
